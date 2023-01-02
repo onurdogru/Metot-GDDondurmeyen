37cf4743b8411bcc6853b76736cc02d888272ee2
Onur <onurdogruiletisim@gmail.com>
2023-01-02 20:30:27 +0300

    Add project files.

---
+++ Metotlar/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metotlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Geriye Döndürmeyen //void = döndürmeyen
+
+        private void temizle()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+            textBox1.Focus();
+        }
+
+        private void Renklendir()
+        {
+            textBox1.BackColor = Color.Green;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Renklendir();
+        }
+    }
+}
